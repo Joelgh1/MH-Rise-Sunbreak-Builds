b4d9280e9db489cd9403e28c3c9b94917c2e2b08
Joel Garrido <joelgamar@gmail.com>
2023-01-17 18:55:35 +0100

    added angular material and styled the armors list

--- src/app/armor-list/armor-list.component.ts
+++ src/app/armor-list/armor-list.component.ts
@@ -7,14 +7,16 @@ import { ArmorServiceService } from '../armor-service.service';
   templateUrl: './armor-list.component.html',
   styleUrls: ['./armor-list.component.css']
 })
-export class ArmorListComponent implements OnInit{
+export class ArmorListComponent implements OnInit {
   armors: Armor[] = []
-  constructor(private armorService: ArmorServiceService){}
+  selectedArmor?: Armor
 
-  filterByRank(rank: String){
+  constructor(private armorService: ArmorServiceService) { }
+
+  filterByRank(rank: String) {
     let filtered: Armor[] = []
     this.armors.forEach((value) => {
-      if(value.rank === rank) filtered.push(value)
+      if (value.rank === rank) filtered.push(value)
     })
     this.armors = filtered
   }
@@ -22,4 +24,20 @@ export class ArmorListComponent implements OnInit{
   ngOnInit(): void {
     this.armors = this.armorService.getArmors()
   }
+
+  changeSelected(armor: Armor) {
+    this.selectedArmor = armor
+  }
+
+  setIcon(armorType: string): string {
+    switch (armorType) {
+      case "HEAD": return "https://static.wikia.nocookie.net/monsterhunter/images/8/88/MHRise_Item_Icon-Helmet_Grey.svg"; break;
+      case "CHEST": return "https://static.wikia.nocookie.net/monsterhunter/images/e/e1/MHRise_Item_Icon-Chestplate_Grey.svg"; break;
+      case "ARMS": return "https://static.wikia.nocookie.net/monsterhunter/images/5/55/MHRise_Item_Icon-Armguard_Grey.svg"; break;
+      case "WAIST": return "https://static.wikia.nocookie.net/monsterhunter/images/8/84/MHRise_Item_Icon-Waist_Grey.svg"; break;
+      case "LEGS": return "https://static.wikia.nocookie.net/monsterhunter/images/3/32/MHRise_Item_Icon-Leggings_Grey.svg"; break;
+      default: return ""
+    }
+  }
+
 }
